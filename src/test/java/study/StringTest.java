0e8d546e780d98664ec6c4cdd6fd3209da976efa
brandnuws <jjws1011@gmail.com>
2023-07-24 01:12:41 +0900

    feat: setTest 요구사항1 ~ 3 수행 + stringTest 수정 -stringTest DisplayName을 조금 수정했습니다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -32,7 +32,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항1: \"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+    @DisplayName("String 요구사항1: \"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
     void split_test_expect_single_string_array() {
         //given
         String targetString = "1";
@@ -47,7 +47,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항2: \"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+    @DisplayName("String 요구사항2: \"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
     void substring_test_expect_removing_brace() {
         //given
         String targetString = "(1,2)";
@@ -60,7 +60,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항3: \"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+    @DisplayName("String 요구사항3-1: \"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
     void charAt_test_expect_getting_index_of_string() {
         //given
         String targetString = "abc";
@@ -73,7 +73,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항3: String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+    @DisplayName("String 요구사항3-2: String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
     void charAt_test_expect_throwing_StringIndexOutOfBoundsException() {
 
         assertThatThrownBy(() -> {
