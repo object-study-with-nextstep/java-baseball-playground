fa497d4d0630369fa45748438ad1ee3c16ad6c5a
yomni <yearsheep@gmail.com>
2023-07-24 20:32:54 +0900

    feat: step1 몰빵 책임으로 일단 구현

--- src/main/java/string/calculator/Disassembler.java
+++ src/main/java/string/calculator/Disassembler.java
@@ -2,6 +2,51 @@ package string.calculator;
 
 public class Disassembler {
     public static int calculate(String mathExpression) {
-        return 0;
+        int calculateResult = 0;
+        int currentInteger;
+        String currentExpression = "";
+
+        String[] integersAndExpressions = mathExpression.split(" ");
+
+        for (String candidate : integersAndExpressions) {
+            if (isInteger(candidate)) {
+                currentInteger = Integer.parseInt(candidate);
+                calculateResult = calculate(calculateResult, currentInteger, currentExpression);
+            } else {
+                currentExpression = candidate;
+            }
+        }
+
+        return calculateResult;
+    }
+
+    private static int calculate(int calculateResult, int currentInteger, String currentExpression) {
+        switch (currentExpression) {
+            case "+":
+                calculateResult = calculateResult + currentInteger;
+                break;
+            case "-":
+                calculateResult = calculateResult - currentInteger;
+                break;
+            case "*":
+                calculateResult = calculateResult * currentInteger;
+                break;
+            case "/":
+                calculateResult = calculateResult / currentInteger;
+                break;
+            default:
+                calculateResult = currentInteger;
+                break;
+        }
+        return calculateResult;
+    }
+
+    private static boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
